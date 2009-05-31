7e7d6a59dfde16a07793132a3ef10e746a83b34c
tim.moxter <tim.moxter@d6820e0a-e98e-11dd-b00f-9fabb4b2ff13>
2009-05-31 09:48:24 +0000

    - tag MxUdpate Update Deployment Tool version V0.4.0 - improvements:   * issue #18 : Post-Update Script must be also defined within Source Code   * issue #21 : New parameter added to compile JPOs automatically

---
+++ mxupdate-update/tags/V0.4.0/src/org/mxupdate/plugin/Update_mxJPO.java
@@ -0,0 +1,250 @@
+/*
+ * Copyright 2008-2009 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.plugin;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import matrix.db.Context;
+import matrix.db.MatrixWriter;
+
+import org.mxupdate.mapping.TypeDef_mxJPO;
+import org.mxupdate.update.AbstractObject_mxJPO;
+import org.mxupdate.update.util.ParameterCache_mxJPO;
+
+/**
+ * The JPO class is the plug-in to create or update configuration items defined
+ * through given MX update files.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class Update_mxJPO
+        extends AbstractPlugin_mxJPO
+{
+    /**
+     * Main method to create or update given TCL update file.
+     *
+     * @param _context  MX context for this request
+     * @param _args     first index of the arguments defined the file to update
+     * @throws Exception if the update failed
+     * @deprecated
+     */
+    @Deprecated
+    public void mxMain(final Context _context,
+                       final String... _args)
+            throws Exception
+    {
+        final Set<File> files = new HashSet<File>();
+        files.add(new File(_args[0]));
+        final MatrixWriter writer = new MatrixWriter(_context);
+        writer.write(this.updateFiles(_context, files, false));
+        writer.flush();
+        writer.close();
+    }
+
+    /**
+     * Updates MX Update files depending on file names. The method is used from
+     * the MX Update Eclipse Plug-In if the MxUpdate Update Deployment tool has
+     * access to the files from the Eclipse Plug-In.
+     *
+     * @param _context  MX context for this request
+     * @param _args     encoded arguments from the Eclipse plug-in:
+     *                  <ul>
+     *                  <li><b>{@link Set}&lt;{@link String}&gt;</b><br/>set of
+     *                      file names which must be updated</li>
+     *                  <li><b>{@link Boolean}</b><br/> if set to <i>true</i>
+     *                      all included JPOs are compiled; otherwise no JPOs
+     *                      are compiled</li>
+     *                  </ul>
+     * @return logging information from the update
+     * @throws Exception if the update failed
+     * @see #updateFiles(Context, Set)
+     */
+    public String updateByName(final Context _context,
+                               final String[] _args)
+            throws Exception
+    {
+        final Set<String> fileNames = this.<Set<String>>decode(_args[0]);
+        final boolean compile = this.<Boolean>decode(_args[1]);
+
+        final Set<File> files = new HashSet<File>();
+        for (final String fileName : fileNames)  {
+            files.add(new File(fileName));
+        }
+
+        return this.updateFiles(_context, files, compile);
+    }
+
+    /**
+     * Updates the files depending on the file name with related file content.
+     * The method is used from the MX Update Eclipse Plug-In if the MxUpdate
+     * Update Deployment tool has <b>NO</b> access to the files from the
+     * Eclipse Plug-In (e.g. if a virtual machine is used, or MX is located on
+     * a server).
+     *
+     * @param _context  MX context for this request
+     * @param _args     encoded arguments from the Eclipse plug-in:
+     *                  <ul>
+     *                  <li><b>{@link Map}&lt;{@link String},{@link String}&gt;
+     *                      </b><br/>map of the file name and the related file
+     *                      content which must be updated</li>
+     *                  <li><b>{@link Boolean}</b><br/> if set to <i>true</i>
+     *                      all included JPOs are compiled; otherwise no JPOs
+     *                      are compiled</li>
+     *                  </ul>
+     * @return logging information from the update
+     * @throws Exception if the update failed
+     * @see #updateFiles(Context, Set)
+     */
+    public String updateByContent(final Context _context,
+                                  final String[] _args)
+            throws Exception
+    {
+        String ret = "";
+
+        final Map<String,String> files = this.<Map<String,String>>decode(_args[0]);
+        final boolean compile = this.<Boolean>decode(_args[1]);
+
+        // create temporary directory
+        final File tmpDir = File.createTempFile("Update", "tmp");
+        tmpDir.delete();
+        tmpDir.mkdir();
+
+        final Set<File> localFiles = new HashSet<File>();
+        try  {
+            // store all files in temporary directory
+            for (final Map.Entry<String,String> fileEntry : files.entrySet())  {
+                final String fileName = (new File(fileEntry.getKey())).getName();
+
+                final File localFile = new File(tmpDir, fileName);
+                final Writer writer = new FileWriter(localFile);
+                writer.write(fileEntry.getValue());
+                writer.close();
+
+                localFiles.add(localFile);
+            }
+
+            // and call update
+            ret = this.updateFiles(_context, localFiles, compile);
+        } finally  {
+            // at least remove all temporary stuff
+            for (final File localFile : localFiles)  {
+                localFile.delete();
+            }
+            tmpDir.delete();
+        }
+
+        return ret;
+    }
+
+    /**
+     * Updates all configurations items specified through given set of MX
+     * update files.
+     *
+     * @param _context  MX context for this request
+     * @param _files    set of all files to update
+     * @param _compile  if <i>true</i> related JPOs are compiled; if
+     *                  <i>false</i> no JPOs are compiled
+     * @return logging information from the update
+     * @throws Exception if update for the <code>_files</code> failed
+     */
+    protected String updateFiles(final Context _context,
+                                 final Set<File> _files,
+                                 final boolean _compile)
+            throws Exception
+    {
+        // initialize mapping
+        final ParameterCache_mxJPO paramCache = new ParameterCache_mxJPO(_context, true);
+
+        // first found related type definition instances
+        final Map<File,AbstractObject_mxJPO> instances = new TreeMap<File,AbstractObject_mxJPO>();
+        for (final File file : _files)  {
+            boolean found = false;
+            for (final TypeDef_mxJPO typeDef : paramCache.getMapping().getAllTypeDefs())  {
+                if (!typeDef.isFileMatchLast())  {
+                    final AbstractObject_mxJPO instance = typeDef.newTypeInstance(null);
+                    final String mxName = instance.extractMxName(paramCache, file);
+                    if (mxName != null)  {
+                        instances.put(file, typeDef.newTypeInstance(mxName));
+                        found  = true;
+                        break;
+                    }
+                }
+            }
+            if (!found)  {
+                for (final TypeDef_mxJPO typeDef : paramCache.getMapping().getAllTypeDefs())  {
+                    if (typeDef.isFileMatchLast())  {
+                        final AbstractObject_mxJPO instance = typeDef.newTypeInstance(null);
+                        final String mxName = instance.extractMxName(paramCache, file);
+                        if (mxName != null)  {
+                            instances.put(file, typeDef.newTypeInstance(mxName));
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // check if objects must be created
+        final Map<TypeDef_mxJPO,Set<String>> allMxNames = new HashMap<TypeDef_mxJPO,Set<String>>();
+        for (final AbstractObject_mxJPO instance : instances.values())  {
+            if (!allMxNames.containsKey(instance.getTypeDef()))  {
+                allMxNames.put(instance.getTypeDef(), instance.getMxNames(paramCache));
+            }
+            if (!allMxNames.get(instance.getTypeDef()).contains(instance.getName()))  {
+                paramCache.logInfo("created " + instance.getTypeDef().getLogging() + " '" + instance.getName() + "'");
+                instance.create(paramCache);
+            }
+        }
+
+        // update
+        for (final Map.Entry<File,AbstractObject_mxJPO> instanceEntry : instances.entrySet())  {
+            paramCache.logInfo("updated " + instanceEntry.getValue().getTypeDef().getLogging()
+                    + " '" + instanceEntry.getValue().getName() + "'");
+            instanceEntry.getValue().update(paramCache, instanceEntry.getKey(), "");
+        }
+
+        // at least compile (to be sure that all JPOs are updated)
+        if (_compile)  {
+            for (final Map.Entry<File,AbstractObject_mxJPO> instanceEntry : instances.entrySet())  {
+                try  {
+                    if (instanceEntry.getValue().compile(paramCache))  {
+                        paramCache.logInfo("compiled " + instanceEntry.getValue().getTypeDef().getLogging()
+                                + " '" + instanceEntry.getValue().getName() + "'");
+                    }
+                } catch (final Exception e)  {
+                    paramCache.logInfo("compile of " + instanceEntry.getValue().getTypeDef().getLogging()
+                            + " '" + instanceEntry.getValue().getName() + "' failed:\n" + e.toString());
+                }
+            }
+        }
+
+        return paramCache.getLogString();
+    }
+}
---
+++ mxupdate-update/tags/V0.4.0/src/org/mxupdate/update/userinterface/Portal_mxJPO.java
@@ -0,0 +1,290 @@
+/*
+ * Copyright 2008-2009 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.update.userinterface;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Stack;
+import java.util.TreeMap;
+
+import matrix.util.MatrixException;
+
+import org.mxupdate.mapping.TypeDef_mxJPO;
+import org.mxupdate.update.AbstractAdminObject_mxJPO;
+import org.mxupdate.update.util.ParameterCache_mxJPO;
+import org.mxupdate.update.util.StringUtil_mxJPO;
+
+/**
+ * The class parses the information about the portal and writes the script used
+ * to update portals.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class Portal_mxJPO
+         extends AbstractAdminObject_mxJPO
+{
+    /**
+     * Defines the serialize version unique identifier.
+     */
+    private static final long serialVersionUID = -3337088092343095556L;
+
+    /**
+     * Alt (label) of the portal.
+     *
+     * @see #writeObject(ParameterCache_mxJPO, Appendable)
+     */
+    private String alt = null;
+
+    /**
+     * Href of the portal.
+     *
+     * @see #writeObject(ParameterCache_mxJPO, Appendable)
+     */
+    private String href = null;
+
+    /**
+     * Label of the portal.
+     *
+     * @see #writeObject(ParameterCache_mxJPO, Appendable)
+     */
+    private String label = null;
+
+    /**
+     * Stack used to parse the channel references.
+     *
+     * @see #parse(String, String)
+     * @see #prepare(ParameterCache_mxJPO)
+     */
+    private final Stack<ChannelRef> channelRefs = new Stack<ChannelRef>();
+
+    /**
+     * Ordered channel references by row and column.
+     *
+     * @see #prepare(ParameterCache_mxJPO)
+     * @see #writeObject(ParameterCache_mxJPO, Appendable)
+     */
+    final Map<Integer,Map<Integer,ChannelRef>> orderedChannelRefs = new TreeMap<Integer,Map<Integer,ChannelRef>>();
+
+    /**
+     * Constructor used to initialize the type definition enumeration.
+     *
+     * @param _typeDef  defines the related type definition enumeration
+     * @param _mxName   MX name of the administration object
+     */
+    public Portal_mxJPO(final TypeDef_mxJPO _typeDef,
+                        final String _mxName)
+    {
+        super(_typeDef, _mxName);
+    }
+
+    /**
+     * Parses the {@link #alt}, {@link #href}, {@link #label} and the channel
+     * reference {@link #channelRefs}.
+     *
+     * @param _url      url of the XML tag
+     * @param _content  content of the XML tag
+     */
+    @Override
+    protected void parse(final String _url,
+                         final String _content)
+    {
+        if ("/alt".equals(_url))  {
+            this.alt = _content;
+        } else if ("/href".equals(_url))  {
+            this.href = _content;
+        } else if ("/label".equals(_url))  {
+            this.label = _content;
+
+        } else if ("/channelRefList".equals(_url))  {
+            // to be ignored ...
+        } else if ("/channelRefList/channelRef".equals(_url))  {
+            this.channelRefs.add(new ChannelRef());
+        } else if ("/channelRefList/channelRef/name".equals(_url))  {
+            this.channelRefs.peek().name = _content;
+        } else if ("/channelRefList/channelRef/portalRow".equals(_url))  {
+            this.channelRefs.peek().row = Integer.parseInt(_content);
+        } else if ("/channelRefList/channelRef/portalColumn".equals(_url))  {
+            this.channelRefs.peek().column = Integer.parseInt(_content);
+
+        } else  {
+            super.parse(_url, _content);
+        }
+    }
+
+    /**
+     * Order the channel references.
+     *
+     * @param _paramCache   parameter cache
+     * @throws MatrixException if the preparation from derived class failed
+     * @see #channelRefs        stack of not ordered channel references
+     * @see #orderedChannelRefs ordered channel references
+     */
+    @Override
+    protected void prepare(final ParameterCache_mxJPO _paramCache)
+            throws MatrixException
+    {
+        // sort the channels by row and column
+        for (final ChannelRef channelRef : this.channelRefs)  {
+            Map<Integer,ChannelRef> sub = this.orderedChannelRefs.get(channelRef.row);
+            if (sub == null)  {
+                sub = new TreeMap<Integer,ChannelRef>();
+                this.orderedChannelRefs.put(channelRef.row, sub);
+            }
+            sub.put(channelRef.column, channelRef);
+        }
+
+        super.prepare(_paramCache);
+    }
+
+    /**
+     * Writes specific information about the cached portal to the given writer
+     * instance. This includes
+     * <ul>
+     * <li>{@link #label}</li>
+     * <li>{@link #href}</li>
+     * <li>{@link #alt}</li>
+     * <li>settings defined as properties starting with &quot;%&quot; in
+     *     {@link #getPropertiesMap()}</li>
+     * <li>channel references {@link #orderedChannelRefs}</li>
+     * </ul>
+     *
+     * @param _paramCache   parameter cache
+     * @param _out          writer instance to the TCL update file
+     * @throws IOException if the TCL update code could not be written
+     */
+    @Override
+    protected void writeObject(final ParameterCache_mxJPO _paramCache,
+                               final Appendable _out)
+            throws IOException
+    {
+        _out.append(" \\\n    label \"").append(StringUtil_mxJPO.convertTcl(this.label)).append("\"");
+        if (this.href != null)  {
+            _out.append(" \\\n    href \"").append(StringUtil_mxJPO.convertTcl(this.href)).append("\"");
+        }
+        if (this.alt != null)  {
+            _out.append(" \\\n    alt \"").append(StringUtil_mxJPO.convertTcl(this.alt)).append("\"");
+        }
+        // settings
+        for (final AbstractAdminObject_mxJPO.Property prop : this.getPropertiesMap().values())  {
+            if (prop.getName().startsWith("%"))  {
+                _out.append(" \\\n    add setting \"")
+                    .append(StringUtil_mxJPO.convertTcl(prop.getName().substring(1))).append("\"")
+                    .append(" \"").append(StringUtil_mxJPO.convertTcl(prop.getValue())).append("\"");
+            }
+        }
+        // channel references
+        boolean firstRow = true;
+        for (final Map<Integer,ChannelRef> channelRefs : this.orderedChannelRefs.values())  {
+            boolean firstCol = true;
+            for (final ChannelRef channelRef : channelRefs.values())  {
+                _out.append(" \\\n    place \"").append(StringUtil_mxJPO.convertTcl(channelRef.name)).append("\"");
+                if (!firstRow && firstCol)  {
+                    _out.append(" newrow");
+                }
+                _out.append(" after \"\"");
+                firstCol = false;
+            }
+            firstRow = false;
+        }
+    }
+
+    /**
+     * The method overwrites the original method to append the MQL statements
+     * in the <code>_preMQLCode</code> to reset this portal. Following steps
+     * are done:
+     * <ul>
+     * <li>reset HRef, description, alt and label</li>
+     * <li>remove all settings and channels</li>
+     * </ul>
+     *
+     * @param _paramCache       parameter cache
+     * @param _preMQLCode       MQL statements which must be called before the
+     *                          TCL code is executed
+     * @param _postMQLCode      MQL statements which must be called after the
+     *                          TCL code is executed
+     * @param _preTCLCode       TCL code which is defined before the source
+     *                          file is sourced
+     * @param _tclVariables     map of all TCL variables where the key is the
+     *                          name and the value is value of the TCL variable
+     *                          (the value is automatically converted to TCL
+     *                          syntax!)
+     * @param _sourceFile       souce file with the TCL code to update
+     * @throws Exception if the update from derived class failed
+     */
+    @Override
+    protected void update(final ParameterCache_mxJPO _paramCache,
+                          final CharSequence _preMQLCode,
+                          final CharSequence _postMQLCode,
+                          final CharSequence _preTCLCode,
+                          final Map<String,String> _tclVariables,
+                          final File _sourceFile)
+            throws Exception
+    {
+        // HRef, description, alt and label
+        final StringBuilder preMQLCode = new StringBuilder()
+                .append("mod ").append(this.getTypeDef().getMxAdminName())
+                .append(" \"").append(this.getName()).append('\"')
+                .append(" href \"\" description \"\" alt \"\" label \"\"");
+
+        // reset settings
+        for (final AbstractAdminObject_mxJPO.Property prop : this.getPropertiesMap().values())  {
+            if (prop.getName().startsWith("%"))  {
+                preMQLCode.append(" remove setting \"").append(prop.getName().substring(1)).append('\"');
+            }
+        }
+        preMQLCode.append(";\n");
+
+        // remove channels (each channel must be removed in a single line...)
+        for (final ChannelRef channelRef : this.channelRefs)  {
+            preMQLCode.append("mod ").append(this.getTypeDef().getMxAdminName())
+                      .append(" \"").append(this.getName()).append('\"')
+                      .append(" remove channel \"").append(channelRef.name).append("\";\n");
+        }
+
+        // append already existing pre MQL code
+        preMQLCode.append(_preMQLCode);
+
+        super.update(_paramCache, preMQLCode, _postMQLCode, _preTCLCode, _tclVariables, _sourceFile);
+    }
+
+    /**
+     * Class holding the channel reference.
+     */
+    private class ChannelRef
+    {
+        /**
+         * Name of the channel.
+         */
+        String name = null;
+
+        /**
+         * Row of the channel.
+         */
+        Integer row = null;
+
+        /**
+         * Column of the channel.
+         */
+        Integer column = null;
+    }
+}
