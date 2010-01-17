649b7bb0e558ab827a927d9e580a508b2dbb6dfb
tim.moxter <tim.moxter@d6820e0a-e98e-11dd-b00f-9fabb4b2ff13>
2010-01-17 21:02:02 +0000

    - release version V0.8.0 - following issues are solved:   * issue #72 Support for Global Integration Definition with derived types in V6   * issue #85 Support for Notification Objects in V6   * issue #86 Export of policies with all formats / types could not be imported   * issue #87 Support of expression filters for public / owner access of policies in V6   * issue #88 Support for revoke access for owner and public for policy states in V6   * issue #89 Support of EBOM sync integration configuration in V6   * issue #91 Support of expression filters for public / owner access of rules in V6   * issue #92 Support for revoke access for owner and public for rules in V6

---
+++ mxupdate-update/tags/V0.8.0/src/main/java/org/mxupdate/update/user/Role_mxJPO.java
@@ -0,0 +1,282 @@
+/*
+ * Copyright 2008-2009 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.update.user;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import matrix.util.MatrixException;
+
+import org.mxupdate.mapping.TypeDef_mxJPO;
+import org.mxupdate.update.util.MqlUtil_mxJPO;
+import org.mxupdate.update.util.ParameterCache_mxJPO;
+import org.mxupdate.update.util.StringUtil_mxJPO;
+
+/**
+ * The class is used to export, create, delete and update roles within MX.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class Role_mxJPO
+    extends AbstractUser_mxJPO
+{
+    /**
+     * Set of all ignored URLs from the XML definition for roles.
+     *
+     * @see #parse(String, String)
+     */
+    private static final Set<String> IGNORED_URLS = new HashSet<String>();
+    static  {
+        Role_mxJPO.IGNORED_URLS.add("/parentRole");
+        // new URIL to be ignored, because read within prepare method
+        Role_mxJPO.IGNORED_URLS.add("/roleType");
+    }
+
+    /**
+     * Name of the parameter to define if current MX version supports role
+     * types.
+     *
+     * @see #prepare(ParameterCache_mxJPO)
+     */
+    private static final String PARAM_SUPPORT_ROLE_TYPES = "UserRoleSupportRoleType";
+
+    /**
+     * Defines the parameter for the match of roles for which workspace objects
+     * are not handled (neither exported nor updated).
+     *
+     * @see #ignoreWorkspaceObjects(ParameterCache_mxJPO)
+     */
+    private static final String PARAM_IGNORE_WSO_ROLES = "UserIgnoreWSO4Roles";
+
+    /**
+     * Set to hold all parent roles.
+     *
+     * @see #parse(String, String)
+     */
+    private final Set<String> parentRoles = new TreeSet<String>();
+
+    /**
+     * Stores the information about the role type.
+     *
+     * @see RoleType
+     * @see #prepare(ParameterCache_mxJPO)
+     */
+    private RoleType roleType = RoleType.ROLE;
+
+    /**
+     * Constructor used to initialize this role definition with related type
+     * definition <code>_typeDef</code> for given <code>_name</code>.
+     *
+     * @param _typeDef  defines the related type definition enumeration
+     * @param _mxName   MX name of the administration object
+     */
+    public Role_mxJPO(final TypeDef_mxJPO _typeDef,
+                      final String _mxName)
+    {
+        super(_typeDef, _mxName);
+    }
+
+    /**
+     * <p>Parses all role specific URL values. This includes:
+     * <ul>
+     * <li>{@link #parentRoles parent roles}</li>
+     * </ul></p>
+     * <p>If an <code>_url</code> is included in {@link #IGNORED_URLS}, this
+     * URL is ignored.</p>
+     *
+     * @param _url      URL to parse
+     * @param _content  content of the URL to parse
+     * @see #IGNORED_URLS
+     * @see #prepare(ParameterCache_mxJPO)
+     */
+    @Override()
+    protected void parse(final String _url,
+                         final String _content)
+    {
+        if (!Role_mxJPO.IGNORED_URLS.contains(_url))  {
+            if ("/parentRole/roleRef".equals(_url))  {
+                this.parentRoles.add(_content);
+            } else  {
+                super.parse(_url, _content);
+            }
+        }
+    }
+
+    /**
+     * Prepares the internal information after the XML export was parsed by
+     * adding the information about the role type (because the role type
+     * information was numbers within the XML string).
+     *
+     * @param _paramCache   parameter cache with the MX context
+     * @throws MatrixException if the information about the role type could not
+     *                         be fetched or if the exception was thrown from
+     *                         called super method
+     * @see #roleType
+     * @see #PARAM_SUPPORT_ROLE_TYPES
+     */
+    @Override()
+    protected void prepare(final ParameterCache_mxJPO _paramCache)
+        throws MatrixException
+    {
+        // must the role type evaluated?
+        if (_paramCache.getValueBoolean(Role_mxJPO.PARAM_SUPPORT_ROLE_TYPES))  {
+            final String testRoleType = MqlUtil_mxJPO.execMql(_paramCache, new StringBuilder()
+                    .append("escape print role \"").append(StringUtil_mxJPO.convertMql(this.getName()))
+                    .append("\" select isanorg isaproject dump"));
+            if ("FALSE,TRUE".equals(testRoleType))  {
+                this.roleType = RoleType.PROJECT;
+            } else if ("TRUE,FALSE".equals(testRoleType)) {
+                this.roleType = RoleType.ORGANIZATION;
+            }
+        }
+
+        super.prepare(_paramCache);
+    }
+
+    /**
+     * Writes specific information about the cached role to the given
+     * writer instance.
+     *
+     * @param _paramCache   parameter cache
+     * @param _out          appendable instance to the TCL update file
+     * @throws IOException if the TCL update code could not written
+     */
+    @Override()
+    protected void writeObject(final ParameterCache_mxJPO _paramCache,
+                               final Appendable _out)
+        throws IOException
+    {
+        super.writeObject(_paramCache, _out);
+
+        // role type
+        switch (this.roleType)  {
+            case PROJECT:
+                _out.append(" \\\n    asaproject");
+                break;
+            case ORGANIZATION:
+                _out.append(" \\\n    asanorg");
+                break;
+            case ROLE:
+            default:
+                // the role is the default value
+        }
+        // parent roles
+        for (final String role : this.parentRoles)  {
+            _out.append("\nmql escape mod role \"")
+                .append(StringUtil_mxJPO.convertTcl(role))
+                .append("\" child \"${NAME}\"");
+        }
+    }
+
+    /**
+     * The method overwrites the original method to append the MQL statements
+     * in the <code>_preMQLCode</code> to reset this role. Following steps are
+     * done:
+     * <ul>
+     * <li>reset description</li>
+     * <li>remove all parent groups</li>
+     * <li>define as &quot;normal&quot; role</li>
+     * </ul>
+     *
+     * @param _paramCache       parameter cache
+     * @param _preMQLCode       MQL statements which must be called before the
+     *                          TCL code is executed
+     * @param _postMQLCode      MQL statements which must be called after the
+     *                          TCL code is executed
+     * @param _preTCLCode       TCL code which is defined before the source
+     *                          file is sourced
+     * @param _tclVariables     map of all TCL variables where the key is the
+     *                          name and the value is value of the TCL variable
+     *                          (the value is automatically converted to TCL
+     *                          syntax!)
+     * @param _sourceFile       souce file with the TCL code to update
+     * @throws Exception if the update from derived class failed
+     */
+    @Override()
+    protected void update(final ParameterCache_mxJPO _paramCache,
+                          final CharSequence _preMQLCode,
+                          final CharSequence _postMQLCode,
+                          final CharSequence _preTCLCode,
+                          final Map<String,String> _tclVariables,
+                          final File _sourceFile)
+        throws Exception
+    {
+        // description and all parents
+        final StringBuilder preMQLCode = new StringBuilder()
+                .append("escape mod ").append(this.getTypeDef().getMxAdminName())
+                .append(" \"").append(StringUtil_mxJPO.convertMql(this.getName())).append('\"')
+                .append(" description \"\"")
+                .append(" remove parent all");
+        // define normal role type
+        if (this.roleType != RoleType.ROLE)  {
+            preMQLCode.append(" asarole");
+        }
+
+        // append already existing pre MQL code
+        preMQLCode.append(";\n")
+                  .append(_preMQLCode);
+
+        super.update(_paramCache, preMQLCode, _postMQLCode, _preTCLCode, _tclVariables, _sourceFile);
+    }
+
+    /**
+     * <p>Calculates if workspace objects for this role are not handled. This
+     * is done by checking if the name of this role matches one of the match
+     * lists defined with parameter {@link #PARAM_IGNORE_WSO_ROLES}. In this
+     * case the the workspace objects are ignored for this role.</p>
+     *
+     * @param _paramCache       parameter cache
+     * @return <i>true</i> if the handling of workspace objects for this role
+     *         is ignored
+     * @see #PARAM_IGNORE_WSO_ROLES
+     */
+    @Override()
+    protected boolean ignoreWorkspaceObjects(final ParameterCache_mxJPO _paramCache)
+    {
+        boolean ignore = super.ignoreWorkspaceObjects(_paramCache);
+        if (!ignore)  {
+            final Collection<String> ignoreMatches = _paramCache.getValueList(Role_mxJPO.PARAM_IGNORE_WSO_ROLES);
+            if (ignoreMatches != null)  {
+                ignore = StringUtil_mxJPO.match(this.getName(), ignoreMatches);
+            }
+        }
+        return ignore;
+    }
+
+    /**
+     * Enumeration for role types.
+     */
+    private enum RoleType
+    {
+        /** Standard case, the role is a &quot;role&quot;. */
+        ROLE,
+        /** The role is a project role. */
+        PROJECT,
+        /** The role is an organizational role. */
+        ORGANIZATION;
+    }
+}
---
+++ mxupdate-update/tags/V0.8.0/src/test/java/org/mxupdate/test/data/program/JPOProgramData.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2008-2009 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.test.data.program;
+
+import matrix.util.MatrixException;
+
+import org.mxupdate.test.AbstractTest;
+
+/**
+ * The class is used to define all JPO program objects used to create / update
+ * and to export.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class JPOProgramData
+    extends AbstractProgramData<JPOProgramData>
+{
+    /**
+     * Start marker for the update code.
+     */
+    private static final String MARK_START
+            = "################################################################################\n"
+            + "# START NEEDED MQL UPDATE FOR THIS JPO PROGRAM                                 #\n"
+            + "################################################################################";
+
+    /**
+     * End marker for the update code.
+     */
+    private static final String MARK_END
+            = "################################################################################\n"
+            + "# END NEEDED MQL UPDATE FOR THIS JPO PROGRAM                                   #\n"
+            + "################################################################################";
+
+    /**
+     * Initializes this JPO program.
+     *
+     * @param _test     related test instance
+     * @param _name     name of the JPO program
+     */
+    public JPOProgramData(final AbstractTest _test,
+                          final String _name)
+    {
+        super(_test, AbstractTest.CI.PRG_JPO, _name);
+    }
+
+    /**
+     * Returns the configuration item file name of this JPO. The configuration
+     * item file name of a JPO excludes the package name and has the suffix of
+     * <code>_mxJPO.java</code>.
+     *
+     * @return file name of a JPO
+     */
+    @Override()
+    public String getCIFileName()
+    {
+        final String ciFileName = super.getCIFileName();
+
+        return ciFileName.replaceAll("\\.tcl$", "")
+                         .replaceAll(".*\\.", "")
+                        + "_mxJPO.java";
+    }
+
+    /**
+     * The related configuration item file is the {@link #code} of the program.
+     *
+     *  @return {@link #code} of the program
+     *  @see #code
+     */
+    @Override()
+    public String ciFile()
+    {
+        final StringBuilder cmd = new StringBuilder()
+           .append(JPOProgramData.MARK_START)
+           .append("\nmql escape mod program ${NAME} ");
+        this.append4CIFileValues(cmd);
+        cmd.append("\n")
+           .append(JPOProgramData.MARK_END)
+           .append("\n");
+
+        final StringBuilder ciFile = new StringBuilder();
+        for (final String line : cmd.toString().split("\n"))  {
+            ciFile.append("//").append(line).append('\n');
+        }
+        ciFile.append('\n').append(this.getCode());
+
+        return ciFile.toString();
+    }
+
+    /**
+     * Creates this JPO program within MX.
+     *
+     * @return this JPO program instance
+     * @throws MatrixException if create of the JPO program failed
+     */
+    @Override()
+    public JPOProgramData create() throws MatrixException
+    {
+        if (!this.isCreated())  {
+            final StringBuilder cmd = new StringBuilder()
+                .append("escape add program \"").append(AbstractTest.convertMql(this.getName()))
+                .append("\" java ")
+                .append("code \"").append(AbstractTest.convertMql(this.getCode().replaceAll("^package .*;", ""))).append("\"");
+            this.append4Create(cmd);
+            cmd.append(";\n")
+               .append("escape add property ").append(this.getSymbolicName())
+               .append(" on program eServiceSchemaVariableMapping.tcl")
+               .append(" to program \"").append(AbstractTest.convertMql(this.getName())).append("\"");
+
+            this.setCreated(true);
+
+            this.getTest().mql(cmd);
+        }
+        return this;
+    }
+}
