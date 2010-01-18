990b1d4de2178285f3f1fde636ac13495c13d777
tim.moxter <tim.moxter@d6820e0a-e98e-11dd-b00f-9fabb4b2ff13>
2010-01-18 20:50:43 +0000

    - release version V0.8.0.1 - following issue is solved:  * issue #93 Define correct version within MxUpdate installation

---
+++ mxupdate-update/tags/V0.8.0.1/src/main/java/org/mxupdate/update/userinterface/Inquiry_mxJPO.java
@@ -0,0 +1,275 @@
+/*
+ * Copyright 2008-2009 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.update.userinterface;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mxupdate.mapping.TypeDef_mxJPO;
+import org.mxupdate.update.AbstractAdminObject_mxJPO;
+import org.mxupdate.update.util.AdminProperty_mxJPO;
+import org.mxupdate.update.util.ParameterCache_mxJPO;
+import org.mxupdate.update.util.StringUtil_mxJPO;
+
+/**
+ * The class is used to export and import / update inquiry configuration items.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class Inquiry_mxJPO
+    extends AbstractAdminObject_mxJPO
+{
+    /**
+     * Defines the parameter for the comment in front of the separator between
+     * TCL update code and the inquiry code.
+     *
+     * @see #writeEnd(ParameterCache_mxJPO, Appendable)
+     */
+    private static final String PARAM_SEPARATOR_COMMENT = "UIInquirySeparatorComment";
+
+    /**
+     * Defines the parameter for the separator between TCL update code and the
+     * inquiry.
+     *
+     * @see #writeEnd(ParameterCache_mxJPO, Appendable)
+     * @see #update(ParameterCache_mxJPO, CharSequence, CharSequence, CharSequence, Map, File)
+     */
+    private static final String PARAM_SEPARATOR_TEXT = "UIInquirySeparatorText";
+
+    /**
+     * Code for the inquiry.
+     *
+     * @see #parse(String, String)
+     * @see #writeEnd(ParameterCache_mxJPO, Appendable)
+     */
+    private String code;
+
+    /**
+     * Format for the inquiry.
+     *
+     * @see #parse(String, String)
+     * @see #writeObject(ParameterCache_mxJPO, Appendable)
+     */
+    private String format;
+
+    /**
+     * Pattern for the inquiry.
+     *
+     * @see #parse(String, String)
+     * @see #writeObject(ParameterCache_mxJPO, Appendable)
+     */
+    private String pattern;
+
+    /**
+     * Constructor used to initialize the inquiry instance.
+     *
+     * @param _typeDef  defines the related type definition enumeration
+     * @param _mxName   MX name of the administration object
+     */
+    public Inquiry_mxJPO(final TypeDef_mxJPO _typeDef,
+                         final String _mxName)
+    {
+        super(_typeDef, _mxName);
+    }
+
+    /**
+     * Parses the code, format and pattern of an inquiry.
+     *
+     * @param _url      URL to parse
+     * @param _content  related content of the URL to parse
+     * @see #code
+     * @see #format
+     * @see #pattern
+     */
+    @Override()
+    protected void parse(final String _url,
+                         final String _content)
+    {
+        if ("/code".equals(_url))  {
+            this.code = _content;
+        } else if ("/fmt".equals(_url))  {
+            this.format = _content;
+        } else if ("/pattern".equals(_url))  {
+            this.pattern = _content;
+        } else  {
+            super.parse(_url, _content);
+        }
+    }
+
+    /**
+     * Writes the inquiry specific information to the TCL update write. The
+     * properties are identified as settings if they starts with a
+     * &quot;%&quot;. This settings are also written.
+     *
+     * @param _paramCache   parameter cache
+     * @param _out          appendable instance to the TCL update file
+     * @throws IOException if the TCL update code for the menu could not be
+     *                     written
+     * @see #pattern
+     * @see #format
+     * @see #getPropertiesMap()
+     */
+    @Override()
+    protected void writeObject(final ParameterCache_mxJPO _paramCache,
+                               final Appendable _out)
+        throws IOException
+    {
+        _out.append(" \\\n    pattern \"").append(StringUtil_mxJPO.convertTcl(this.pattern)).append("\"")
+            .append(" \\\n    format \"").append(StringUtil_mxJPO.convertTcl(this.format)).append("\"")
+            .append(" \\\n    file [file join \"${FILE}\"]");
+        for (final AdminProperty_mxJPO prop : this.getPropertiesMap().values())  {
+            if (prop.isSetting())  {
+                _out.append(" \\\n    add argument \"")
+                    .append(StringUtil_mxJPO.convertTcl(prop.getName().substring(1))).append("\"")
+                    .append(" \"").append(StringUtil_mxJPO.convertTcl(prop.getValue())).append("\"");
+            }
+        }
+    }
+
+    /**
+     * At the end of the TCL update file the inquiry code must be appended.
+     *
+     * @param _paramCache   parameter cache
+     * @param _out          appendable instance to the TCL update file
+     * @throws IOException if the extension could not be written
+     * @see #code
+     * @see #PARAM_SEPARATOR_COMMENT
+     * @see #PARAM_SEPARATOR_TEXT
+     */
+    @Override()
+    protected void writeEnd(final ParameterCache_mxJPO _paramCache,
+                            final Appendable _out)
+        throws IOException
+    {
+        _out.append("\n\n").append(_paramCache.getValueString(Inquiry_mxJPO.PARAM_SEPARATOR_COMMENT))
+            .append('\n').append(_paramCache.getValueString(Inquiry_mxJPO.PARAM_SEPARATOR_TEXT)).append("\n\n");
+        if (this.code != null)  {
+            _out.append(this.code);
+        }
+    }
+
+    /**
+     * Updates this inquiry. Because the TCL source code of an inquiry includes
+     * also the inquiry code itself, this inquiry code must be separated and
+     * written in a temporary file. This temporary file is used while the
+     * update is running (defined via TCL variable <code>FILE</code>). After
+     * the update, the temporary file is removed (because not needed anymore).
+     * Also the MQL statements to reset this inquiry are appended to the
+     * statements in <code>_preMQLCode</code> to:
+     * <ul>
+     * <li>reset the description, pattern, format and code</li>
+     * <li>remove all arguments</li>
+     * </ul>
+     *
+     * @param _paramCache       parameter cache
+     * @param _preMQLCode       MQL statements which must be called before the
+     *                          TCL code is executed
+     * @param _postMQLCode      MQL statements which must be called after the
+     *                          TCL code is executed
+     * @param _preTCLCode       TCL code which is defined before the source
+     *                          file is sourced
+     * @param _tclVariables     map of all TCL variables where the key is the
+     *                          name and the value is value of the TCL variable
+     *                          (the value is automatically converted to TCL
+     *                          syntax!)
+     * @param _sourceFile       souce file with the TCL code to update
+     * @throws Exception if the update from derived class failed
+     * @see #PARAM_SEPARATOR_TEXT
+     */
+    @Override()
+    protected void update(final ParameterCache_mxJPO _paramCache,
+                          final CharSequence _preMQLCode,
+                          final CharSequence _postMQLCode,
+                          final CharSequence _preTCLCode,
+                          final Map<String,String> _tclVariables,
+                          final File _sourceFile)
+        throws Exception
+    {
+        // reset HRef, description, alt, label and height
+        final StringBuilder preMQLCode = new StringBuilder()
+                .append("escape mod ").append(this.getTypeDef().getMxAdminName())
+                .append(" \"").append(StringUtil_mxJPO.convertMql(this.getName())).append('\"')
+                .append(" description \"\" pattern \"\" format \"\" code \"\"");
+
+        // reset arguments
+        for (final AdminProperty_mxJPO prop : this.getPropertiesMap().values())  {
+            if (prop.isSetting())  {
+                preMQLCode.append(" remove argument \"")
+                          .append(StringUtil_mxJPO.convertMql(prop.getName().substring(1))).append('\"');
+            }
+        }
+
+        // append already existing pre MQL code
+        preMQLCode.append(";\n")
+                  .append(_preMQLCode);
+
+        // separate the inquiry code and the TCL code
+        final String sep = _paramCache.getValueString(Inquiry_mxJPO.PARAM_SEPARATOR_TEXT);
+        final StringBuilder orgCode = this.getCode(_sourceFile);
+        final int idx = orgCode.lastIndexOf(sep);
+        final CharSequence code = (idx >= 0)
+                                  ? orgCode.subSequence(0, idx)
+                                  : orgCode;
+        final CharSequence inqu = (idx >= 0)
+                                  ? orgCode.subSequence(idx + sep.length() + 1,
+                                                        orgCode.length())
+                                  : "";
+
+        final File tmpTclFile = File.createTempFile("TMP_", ".tcl");
+        try  {
+            final File tmpInqFile = File.createTempFile("TMP_", ".inquiry");
+            try  {
+                // write TCL code
+                final Writer outTCL = new FileWriter(tmpTclFile);
+                try {
+                    outTCL.append(code.toString().trim());
+                } finally  {
+                    outTCL.close();
+                }
+
+                // write inquiry code
+                final Writer outInq = new FileWriter(tmpInqFile);
+                try {
+                    outInq.append(inqu.toString().trim());
+                } finally  {
+                    outInq.close();
+                }
+
+                // define TCL variable for the file
+                final Map<String,String> tclVariables = new HashMap<String,String>();
+                tclVariables.putAll(_tclVariables);
+                tclVariables.put("FILE", tmpInqFile.getPath());
+
+                // and update
+                super.update(_paramCache, preMQLCode, _postMQLCode, code, tclVariables, tmpTclFile);
+            } finally  {
+                tmpInqFile.delete();
+            }
+        } finally  {
+            tmpTclFile.delete();
+        }
+    }
+}
---
+++ mxupdate-update/tags/V0.8.0.1/src/test/java/org/mxupdate/test/ci/userinterface/CommandTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2008-2010 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.test.ci.userinterface;
+
+import matrix.util.MatrixException;
+
+import org.mxupdate.test.AbstractDataExportUpdate;
+import org.mxupdate.test.AbstractTest;
+import org.mxupdate.test.ExportParser;
+import org.mxupdate.test.data.user.AbstractUserData;
+import org.mxupdate.test.data.user.PersonAdminData;
+import org.mxupdate.test.data.user.RoleData;
+import org.mxupdate.test.data.userinterface.CommandData;
+import org.mxupdate.test.data.util.PropertyDef;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Test cases for the export of commands.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class CommandTest
+    extends AbstractDataExportUpdate<CommandData>
+{
+    /**
+     * Creates for given <code>_name</code> a new command instance.
+     *
+     * @param _name     name of the command instance
+     * @return command instance
+     */
+    @Override()
+    protected CommandData createNewData(final String _name)
+    {
+        return new CommandData(this, _name);
+    }
+
+    /**
+     * Data provider for test commands.
+     *
+     * @return object array with all test commands
+     */
+    @DataProvider(name = "commands")
+    public Object[][] getCommands()
+    {
+        return this.prepareData("command",
+                new Object[]{
+                        "command without anything (to test required fields)",
+                        new CommandData(this, "hello \" test")},
+                new Object[]{
+                        "complex command with settings",
+                        new CommandData(this, "hello \" test")
+                                .setValue("label", "command label \" \\ ' #")
+                                .setValue("description", "\"\\\\ hallo")
+                                .setValue("href", "${COMMON_DIR}/emxTree.jsp?mode=insert")
+                                .setValue("alt", "${COMMON_DIR}/emxTreeAlt.jsp?mode=insert")
+                                .setSetting("Setting 1", "Setting Value ' 1")
+                                .setSetting("Setting 2", "Value2")},
+                new Object[]{
+                        "complex command with users",
+                        new CommandData(this, "command")
+                                .setValue("label", "aaa.bbb.ccc")
+                                .setValue("description", "\"\\\\ hallo")
+                                .setValue("href", "${COMMON_DIR}/emxTree.jsp?mode=insert")
+                                .setValue("alt", "${COMMON_DIR}/emxTreeAlt.jsp?mode=insert")
+                                .setSetting("Setting 1", "Setting Value ' 1")
+                                .addUser(new RoleData(this, "assigned \"role\""))
+                                .addUser(new PersonAdminData(this, "assigned \"person\""))});
+    }
+
+    /**
+     * Cleanup all test commands.
+     *
+     * @throws MatrixException if cleanup failed
+     */
+    @BeforeMethod()
+    @AfterClass()
+    public void cleanup()
+        throws MatrixException
+    {
+        this.cleanup(AbstractTest.CI.UI_COMMAND);
+        this.cleanup(AbstractTest.CI.USR_ROLE);
+        this.cleanup(AbstractTest.CI.USR_PERSONADMIN);
+    }
+
+    /**
+     * Tests a new created command and the related export.
+     *
+     * @param _description  description of the test case
+     * @param _command      command to test
+     * @throws Exception if test failed
+     */
+    @Test(dataProvider = "commands", description = "test export of new created commands")
+    public void testExport(final String _description,
+                           final CommandData _command)
+        throws Exception
+    {
+        _command.create();
+        _command.checkExport(_command.export());
+    }
+
+    /**
+     * Tests an update of non existing command. The result is tested with by
+     * exporting the command and checking the result.
+     *
+     * @param _description  description of the test case
+     * @param _command      command to test
+     * @throws Exception if test failed
+     */
+    @Test(dataProvider = "commands", description = "test update of non existing command")
+    public void testUpdate(final String _description,
+                           final CommandData _command)
+        throws Exception
+    {
+        // create referenced property value
+        for (final PropertyDef prop : _command.getProperties())  {
+            if (prop.getTo() != null)  {
+                prop.getTo().create();
+            }
+        }
+        // create assigned users
+        for (final AbstractUserData<?> user : _command.getUsers())  {
+            user.create();
+        }
+
+        // first update with original content
+        this.update(_command);
+        final ExportParser exportParser = _command.export();
+        _command.checkExport(exportParser);
+
+        // second update with delivered content
+        this.update(_command.getCIFileName(), exportParser.getOrigCode());
+        _command.checkExport(_command.export());
+    }
+}
