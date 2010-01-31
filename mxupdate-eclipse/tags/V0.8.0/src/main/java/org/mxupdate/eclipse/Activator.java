f89be50decf96901f473581eb6a943c86ccc0610
tim.moxter <tim.moxter@d6820e0a-e98e-11dd-b00f-9fabb4b2ff13>
2010-01-31 23:37:23 +0000

    - release V 0.8.0 - following issues are solved:   * issue #94 maven is used as build tool   * issue #95 external property files could be used for connection settings

---
+++ mxupdate-eclipse/tags/V0.8.0/src/main/java/org/mxupdate/eclipse/Activator.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2008-2010 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.eclipse;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.commons.codec.binary.Base64;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.mxupdate.eclipse.console.Console;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the MxUpdate plug-in life cycle.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class Activator
+    extends AbstractUIPlugin
+{
+
+    /**
+     * Map used to hold the images for MxUpdate files. The first key is the
+     * file extension, the second key the file prefix.
+     */
+    public static final Map<String,Map<String,ImageDescriptor>> IMAGEMAP
+            = new HashMap<String,Map<String,ImageDescriptor>>();
+
+    /**
+     * ID of the MxUpdate plug-in.
+     */
+    public static final String PLUGIN_ID = "MxUpdateEclipsePlugIn"; //$NON-NLS-1$
+
+    /**
+     * Shared instance of the MxUpdate plug-in.
+     */
+    private static Activator plugIn;
+
+    /**
+     * MxUpdate plug-in console.
+     *
+     * @see #showConsole()
+     */
+    private Console console = null;
+
+    private IDeploymentAdapter adapter;
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+    @Override
+    public void start(final BundleContext _context)
+            throws Exception
+    {
+try  {
+        super.start(_context);
+        Activator.plugIn = this;
+
+        this.console = new Console();
+        this.console.activate();
+        ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[]{this.console});
+
+final Properties properties = new Properties();
+final String propStr = this.getPreferenceStore().getString("pluginProperties"); //$NON-NLS-1$
+if (propStr != null)  {
+    final InputStream is = new ByteArrayInputStream(propStr.getBytes());
+    properties.load(is);
+}
+
+// extract all admin type names
+final Set<String> admins = new HashSet<String>();
+for (final Object keyObj : properties.keySet())  {
+    final String key = keyObj.toString().replaceAll("\\..*", ""); //$NON-NLS-1$ //$NON-NLS-2$
+    admins.add(key);
+}
+
+// prepare image cache
+for (final String admin : admins)  {
+    final String prefix = properties.getProperty(admin + ".FilePrefix"); //$NON-NLS-1$
+    final String suffix = properties.getProperty(admin + ".FileSuffix"); //$NON-NLS-1$
+    final String iconStr = properties.getProperty(admin + ".Icon"); //$NON-NLS-1$
+
+    final byte[] bin = Base64.decodeBase64(iconStr.getBytes());
+    final InputStream in = new ByteArrayInputStream(bin);
+
+    final ImageData ret = new ImageData(in);
+
+
+    Map<String,ImageDescriptor> mapPrefix = Activator.IMAGEMAP.get(suffix);
+    if (mapPrefix == null)  {
+        mapPrefix = new HashMap<String,ImageDescriptor>();
+        Activator.IMAGEMAP.put(suffix, mapPrefix);
+    }
+    mapPrefix.put(prefix, ImageDescriptor.createFromImageData(ret));
+}
+} catch (final Throwable e)  {
+    e.printStackTrace(System.out);
+    this.console.logError("ERROR", e); //$NON-NLS-1$
+}
+this.adapter = new MXAdapter(this.getPreferenceStore(), this.console);
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(final BundleContext _context)
+            throws Exception
+    {
+        ConsolePlugin.getDefault().getConsoleManager().removeConsoles(new IConsole[]{ this.console });
+        this.console = null;
+        Activator.plugIn = null;
+        super.stop(_context);
+    }
+
+    /**
+     * Returns the default shared instance of the MxUpdate eclipse plug-in.
+     *
+     * @return shared instance
+     * @see #plugIn
+     */
+    public static Activator getDefault()
+    {
+        return Activator.plugIn;
+    }
+
+    /**
+     * Returns the console for the plug-in. The method is the getter method for
+     * instance variable {@link #console}.
+     *
+     * @return console of the plug-in
+     * @see #console
+     */
+    public Console getConsole()
+    {
+        return this.console;
+    }
+
+    /**
+     * Shows the MxUpdate plug-in console.
+     *
+     * @see #console    MxUpdate plug-in console
+     */
+    public void showConsole()
+    {
+        ConsolePlugin.getDefault().getConsoleManager().showConsoleView(this.console);
+    }
+
+    public IDeploymentAdapter getAdapter()
+    {
+        return this.adapter;
+    }
+}
---
+++ mxupdate-eclipse/tags/V0.8.0/src/main/java/org/mxupdate/eclipse/MXPreferencePage.java
@@ -0,0 +1,291 @@
+/*
+ * Copyright 2008-2010 The MxUpdate Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.mxupdate.eclipse;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FileFieldEditor;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * Common preference page for the MxUpdate eclipse plug-in to define URL, name
+ * and password for the MX context.
+ *
+ * @author The MxUpdate Team
+ * @version $Id$
+ */
+public class MXPreferencePage
+    extends FieldEditorPreferencePage
+    implements IWorkbenchPreferencePage
+{
+    /**
+     * String field editor for the host URL.
+     */
+    private StringFieldEditor host;
+
+    /**
+     * String field editor for the user name.
+     */
+    private StringFieldEditor userName;
+
+    /**
+     * Password field editor.
+     */
+    private PasswordFieldEditor password;
+
+    /**
+     * Flag that the password must be stored.
+     */
+    private BooleanFieldEditor storePassword;
+
+    /**
+     * Flag that the update is done with the file content.
+     */
+    private BooleanFieldEditor updateByFileContent;
+
+    /**
+     * File selector for the property file.
+     */
+    private FileFieldEditor fileEditor;
+
+    /**
+     * Editor for the host (URL) property key.
+     */
+    private StringFieldEditor propKeyHost;
+
+    /**
+     * Editor for the user name property key.
+     */
+    private StringFieldEditor propKeyUserName;
+
+    /**
+     * Editor for the password property key.
+     */
+    private StringFieldEditor propKeyPassword;
+
+    /**
+     * Initialize the layout of the preference (defined as grid) and defines
+     * the preference store from the plug-in activator {@link Activator}.
+     */
+    public MXPreferencePage()
+    {
+        super(FieldEditorPreferencePage.GRID);
+        this.setPreferenceStore(Activator.getDefault().getPreferenceStore());
+    }
+
+    /**
+     *
+     * @see FieldEditorPreferencePage#createFieldEditors()
+     */
+    @Override()
+    protected void createFieldEditors()
+    {
+        final Composite parent = this.getFieldEditorParent();
+
+        this.host = new StringFieldEditor(
+                MXAdapter.PREF_URL,
+                Messages.getString("MXPreferencePage.Host"), //$NON-NLS-1$
+                parent);
+        this.addField(this.host);
+
+        this.userName = new StringFieldEditor(
+                MXAdapter.PREF_NAME,
+                Messages.getString("MXPreferencePage.UserName"), //$NON-NLS-1$
+                parent);
+        this.addField(this.userName);
+
+        this.password = new PasswordFieldEditor(
+                MXAdapter.PREF_PASSWORD,
+                Messages.getString("MXPreferencePage.Password"), //$NON-NLS-1$
+                parent);
+        this.addField(this.password);
+
+        this.storePassword = new BooleanFieldEditor(
+                MXAdapter.PREF_STORE_PASSWORD,
+                Messages.getString("MXPreferencePage.StorePassword"), //$NON-NLS-1$
+                parent);
+        this.addField(this.storePassword);
+
+        this.updateByFileContent = new BooleanFieldEditor(
+                MXAdapter.PREF_UPDATE_FILE_CONTENT,
+                Messages.getString("MXPreferencePage.UpdateByFileContent"), //$NON-NLS-1$
+                parent);
+        this.addField(this.updateByFileContent);
+
+        final BooleanFieldEditor externalConfigured = new BooleanFieldEditor(
+                MXAdapter.PREF_EXTERNAL_CONFIGURED,
+                Messages.getString("MXPreferencePage.ExternalConfigured"), //$NON-NLS-1$
+                parent)
+        {
+            /**
+             * Called if the preference page is called the first time.
+             */
+            @Override()
+            protected void doLoad()
+            {
+                super.doLoad();
+                MXPreferencePage.this.updateDependencies(this.getBooleanValue());
+            }
+            /**
+             * Called if the user (de-) selects the external configuration
+             * flag.
+             *
+             * @param _oldValue     old value
+             * @param _newValue     new value
+             */
+            @Override()
+            protected void valueChanged(final boolean _oldValue,
+                                        final boolean _newValue)
+            {
+                super.valueChanged(_oldValue, _newValue);
+                MXPreferencePage.this.updateDependencies(_newValue);
+            }
+
+        };
+        this.addField(externalConfigured);
+
+        this.fileEditor = new FileFieldEditor(
+                MXAdapter.PREF_PROP_FILE,
+                Messages.getString("MXPreferencePage.PropertyFile"), //$NON-NLS-1$
+                parent);
+        this.fileEditor.setFileExtensions(new String[]{"properties", "*"});
+        this.addField(this.fileEditor);
+
+        this.propKeyHost = new StringFieldEditor(
+                MXAdapter.PREF_PROP_KEY_URL,
+                Messages.getString("MXPreferencePage.PropKeyHost"), //$NON-NLS-1$
+                parent);
+        this.addField(this.propKeyHost);
+
+        this.propKeyUserName = new StringFieldEditor(
+                MXAdapter.PREF_PROP_KEY_NAME,
+                Messages.getString("MXPreferencePage.PropKeyUserName"), //$NON-NLS-1$
+                parent);
+        this.addField(this.propKeyUserName);
+
+        this.propKeyPassword = new StringFieldEditor(
+                MXAdapter.PREF_PROP_KEY_PASSWORD,
+                Messages.getString("MXPreferencePage.PropKeyPassword"), //$NON-NLS-1$
+                parent);
+        this.addField(this.propKeyPassword);
+    }
+
+    /**
+     * Enables / disables the preferences depending on the flag if properties
+     * from external files are read.
+     *
+     * @param _enable   <i>true</i> if configured in external file
+     */
+    protected void updateDependencies(final boolean _enable)
+    {
+        final Composite parent = this.getFieldEditorParent();
+
+        // enable / disable internal configured
+        this.host.setEnabled(!_enable, parent);
+        this.userName.setEnabled(!_enable, parent);
+        this.password.setEnabled(!_enable, parent);
+        this.storePassword.setEnabled(!_enable, parent);
+        this.updateByFileContent.setEnabled(!_enable, parent);
+        // enable / disable external configured
+        this.fileEditor.setEnabled(_enable, parent);
+        this.propKeyHost.setEnabled(_enable, parent);
+        this.propKeyUserName.setEnabled(_enable, parent);
+        this.propKeyPassword.setEnabled(_enable, parent);
+    }
+
+    /**
+     * @param _workbench    workbench which must be initialized
+     * @see IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+     */
+    public void init(final IWorkbench _workbench)
+    {
+    }
+
+    /**
+     * A field editor for a password type preference. This is essentially
+     * a StringFieldEditor, but will replace each character in the input
+     * box with an '*'.
+     */
+    private static class PasswordFieldEditor
+        extends StringFieldEditor
+    {
+        /**
+         * The text field, or <code>null</code> if none.
+         */
+        private Text textField;
+
+        /**
+         * Creates a password field editor of unlimited width.
+         *
+         * @param _name         name of the preference this field editor works
+         *                      on
+         * @param _labelText    label text of the field editor
+         * @param _parent       parent of the field editor's control
+         */
+        public PasswordFieldEditor(final String _name,
+                                   final String _labelText,
+                                   final Composite _parent)
+        {
+            super(_name, _labelText, _parent);
+        }
+
+        /**
+         * Returns this field editor's text control. The control is created if
+         * it does not yet exist
+         *
+         * @param _parent       parent composite
+         * @return text control
+         */
+        @Override()
+        public Text getTextControl(final Composite _parent)
+        {
+            if (this.textField == null) {
+                this.textField = new Text(_parent, SWT.PASSWORD | SWT.SINGLE | SWT.BORDER);
+                this.textField.setFont(_parent.getFont());
+                this.textField.addKeyListener(new KeyAdapter()  {
+                    @Override
+                    public void keyReleased(final KeyEvent _event)
+                    {
+                        MXPreferencePage.PasswordFieldEditor.this.valueChanged();
+                    }
+                });
+                this.textField.addDisposeListener(new DisposeListener()  {
+                    public void widgetDisposed(final DisposeEvent _event)
+                    {
+                        MXPreferencePage.PasswordFieldEditor.this.textField = null;
+                    }
+                });
+            } else  {
+                this.checkParent(this.textField, _parent);
+            }
+            return this.textField;
+        }
+    }
+}
