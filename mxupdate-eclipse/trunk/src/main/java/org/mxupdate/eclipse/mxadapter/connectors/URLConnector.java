0f3dbfde9efacdc6ce8d42d33a548bb063864dd0
tim.moxter <tim.moxter@d6820e0a-e98e-11dd-b00f-9fabb4b2ff13>
2010-03-27 13:22:36 +0000

    - related to issue #106 - the URL connector works now also in the case of errors as expected (throwing exception in this case) - add test cases for the MX URL connector

--- mxupdate-eclipse/trunk/src/main/java/org/mxupdate/eclipse/mxadapter/connectors/URLConnector.java
+++ mxupdate-eclipse/trunk/src/main/java/org/mxupdate/eclipse/mxadapter/connectors/URLConnector.java
@@ -32,10 +32,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.Vector;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IPath;
 import org.mxupdate.eclipse.Activator;
 import org.mxupdate.eclipse.util.CommunicationUtil;
 import org.osgi.framework.Bundle;
@@ -82,6 +82,25 @@ public class URLConnector
     private final Writer out;
 
     /**
+     * Handler for the input stream.
+     *
+     * @see InputStreamHandler
+     */
+    private final InputStreamHandler inHandler;
+
+    /**
+     * Handler for the error stream.
+     *
+     * @see ErrorStreamHandler
+     */
+    private final ErrorStreamHandler errHandler;
+
+    /**
+     * Flag to store that the connection was correct.
+     */
+    private final boolean connected;
+
+    /**
      * Initializes the URL connector to MX database. First the required
      * {@link #SERVER_CLASSES Java classes}Êand {@link #CODEC_LIB codec library}
      * are copied to the <code>_project</code> temporary directory. Then the
@@ -97,7 +116,7 @@ public class URLConnector
      * @param _passwd               MX password
      * @param _updateByFileContent  <i>true</i> if update is done with file
      *                              content
-     * @throws IOException if URL connector could not be initialized
+     * @throws Exception if connect to given MX server is not possible
      */
     public URLConnector(final IProject _project,
                         final String _javaPath,
@@ -106,33 +125,65 @@ public class URLConnector
                         final String _user,
                         final String _passwd,
                         final boolean _updateByFileContent)
-        throws IOException
+        throws Exception
+    {
+        this(Activator.getDefault().getStateLocation().append(_project.getName()).toFile(),
+                Activator.getDefault().getBundle(),
+                _javaPath,
+                _mxJarPath,
+                _url,
+                _user,
+                _passwd,
+                _updateByFileContent);
+    }
+
+    /**
+     * Initializes the connection to the MX server.
+     *
+     * @param _projectPath          path to the project temporary folder
+     * @param _bundle               bundle of the plug-in to access the class
+     *                              and required libraries for the server
+     * @param _javaPath             path for the Java executable
+     * @param _mxJarPath            path of the MX Jar library
+     * @param _url                  URL of the MX server
+     * @param _user                 name of the user on the MX server
+     * @param _passwd               password of the user on the MX server
+     * @param _updateByFileContent  <i>true</i> if update is done by
+     *                              transmitting the file content; otherwise
+     *                              <i>false</i>
+     * @throws Exception if connection to the MX server could not be started
+     */
+    public URLConnector(final File _projectPath,
+                        final Bundle _bundle,
+                        final String _javaPath,
+                        final String _mxJarPath,
+                        final String _url,
+                        final String _user,
+                        final String _passwd,
+                        final boolean _updateByFileContent)
+        throws Exception
     {
         super(_updateByFileContent);
 
-        // copy the required classes and JAR library to temp directory
-        final IPath path = Activator.getDefault().getStateLocation().append(_project.getName());
-        if (!path.toFile().exists())  {
-            path.toFile().mkdirs();
+        // copy the required classes and JAR library to temporary project dir.
+        if (!_projectPath.exists())  {
+            _projectPath.mkdirs();
         }
-
-        final Bundle bundle = Activator.getDefault().getBundle();
         for (final Class<?> clazz : URLConnector.SERVER_CLASSES)  {
             final String clazzFileName = "/" + clazz.getName().replace('.', File.separatorChar) + ".class";
 
             final URL url;
             // must be checked to differ between development and productive
-            if (bundle.getEntryPaths("/bin") != null)  {
-                url = bundle.getEntry("/bin" + clazzFileName);
+            if (_bundle.getEntryPaths("/bin") != null)  {
+                url = _bundle.getEntry("/bin" + clazzFileName);
             } else  {
-                url = bundle.getEntry(clazzFileName);
+                url = _bundle.getEntry(clazzFileName);
             }
 
-            this.copy(url, new File(path.toFile(), "/bin" + clazzFileName));
+            this.copy(url, new File(_projectPath, "/bin" + clazzFileName));
         }
-
-        this.copy(Activator.getDefault().getBundle().getEntry("/lib/" + URLConnector.CODEC_LIB),
-                  new File(path.toFile(), "/lib/" + URLConnector.CODEC_LIB));
+        this.copy(_bundle.getEntry("/lib/" + URLConnector.CODEC_LIB),
+                  new File(_projectPath, "/lib/" + URLConnector.CODEC_LIB));
 
         // prepare class path
         final StringBuilder classPath = new StringBuilder()
@@ -150,93 +201,122 @@ public class URLConnector
                 _url,
                 _user,
                 _passwd);
-        pb.directory(path.toFile());
+        pb.directory(_projectPath);
+
         this.process = pb.start();
 
         this.out = new OutputStreamWriter(this.process.getOutputStream());
+        this.inHandler = new InputStreamHandler(this.process.getInputStream());
+        this.errHandler = new ErrorStreamHandler(this.process.getErrorStream());
+        new Thread(this.inHandler).start();
+        new Thread(this.errHandler).start();
 
-//        Thread.sleep(5000);
-
-//        final Reader in = new InputStreamReader(process.getInputStream());
-//        final Reader err = new InputStreamReader(process.getErrorStream());
-/*
-final Map<String,String> submit = new HashMap<String,String>();
-submit.put("method", "myTestMethod");
-
-String encoded = CommunicationUtil.encode(submit);
-
-
-        final Writer out = new OutputStreamWriter(process.getOutputStream());
-out.write(String.valueOf(encoded.length()));
-out.write(' ');
-out.write(encoded);
-out.write(' ');
-out.flush();
-
-final Map<String,?> bck = CommunicationUtil.readParams(process.getInputStream());
-
-*/
-
-/*        Integer exitCode = null;
-boolean first = true;
-        for (;;) {
-            if (err.ready())  {
-                System.err.print((char) err.read());
-            } else if (in.ready())  {
-                System.out.print((char) in.read());
-            } else  {
-if (first)  {
-    first = false;
-}
-                try {
-                    exitCode = process.exitValue();
-                    break;
-                } catch (final IllegalThreadStateException e)  {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (final InterruptedException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    }
-                }
+        try  {
+            this.testConnection();
+            this.connected = true;
+        } finally  {
+            if (!this.connected)  {
+                this.disconnect();
             }
         }
-*/
-        // (exitCode != null) && (exitCode == 0);
+    }
 
+    /**
+     * Calls the test method on the server to be sure the connection to MX
+     * works as expected.
+     *
+     * @throws Exception if connect failed or if execute of the test on the
+     *                   server process failed
+     * @see #execute(String, String, String, String)
+     */
+    private void testConnection()
+        throws Exception
+    {
+        if (!"connect".equals(this.execute("test", null, null, null)))  {
+            throw new Exception("connect failed");
+        }
     }
 
     /**
      * {@inheritDoc}
+     * Internally {@link #execute(String, String, String, String)} is executed
+     * for the &quot;<code>dispatch</code> method.
      *
-     * @throws IOException              if the {@link #out}Êstream could not be
-     *                                  written
-     * @throws ClassNotFoundException   if returned value from the server could
-     *                                  not be decoded
-     * @throws InterruptedException     if the {@link #process} is interrupted
+     * @see #execute(String, String, String, String)
      */
     public String execute(final String _arg1,
                           final String _arg2,
                           final String _arg3)
-        throws IOException, ClassNotFoundException, InterruptedException
+        throws Exception
     {
+        return this.execute("dispatch", _arg1, _arg2, _arg3);
+    }
+
+    /**
+     *
+     * @param _method   name of the method on the server to call
+     * @param _arg1     first argument
+     * @param _arg2     second argument
+     * @param _arg3     third argument
+     * @return returns string from the server
+     * @throws Exception {@link IOException} if the {@link #out}Êstream could
+     *                   not be written,
+     *                   {@link ClassNotFoundException} if returned value from
+     *                   the server could not be decoded,
+     *                   {@link InterruptedException} if the {@link #process} is interrupted
+     */
+    protected String execute(final String _method,
+                             final String _arg1,
+                             final String _arg2,
+                             final String _arg3)
+        throws Exception
+    {
+        // prepare parameters
         final Map<Integer,String> args = new HashMap<Integer,String>();
-        args.put(0, "dispatch");
+        args.put(0, _method);
         args.put(1, _arg1);
         args.put(2, _arg2);
         args.put(3, _arg3);
         final String encoded = CommunicationUtil.encode(args);
 
+        // write to output stream
         this.out.write(String.valueOf(encoded.length()));
         this.out.write(' ');
         this.out.write(encoded);
         this.out.write(' ');
         this.out.flush();
 
-        final Map<String,?> bck = CommunicationUtil.readParams(this.process.getInputStream());
+        // wait for return
+        final int length = Integer.valueOf(this.readOneWord());
+        final String value = this.readOneWord();
+        if (value.length() != length)  {
+            throw new Exception("wrong parameters");
+        }
+        final Map<String,?> bck = CommunicationUtil.decode(value);
 
+        // return found values
         return (String) bck.get("ret");
     }
+
+    /**
+     * Reads next word from the input stream.
+     *
+     * @return read word
+     * @throws Exception    if an error has occurred while reading next word
+     *                      this error will be thrown
+     */
+    private String readOneWord()
+        throws Exception
+    {
+        while (!this.inHandler.wordExists() && this.errHandler.isEmpty())  {
+            Thread.sleep(1000);
+        }
+        if (!this.errHandler.isEmpty())  {
+            throw new Exception(this.errHandler.read());
+        }
+        return this.inHandler.readWord();
+    }
+
 
     /**
      * {@inheritDoc}
@@ -254,27 +334,31 @@ if (first)  {
 
         final String encoded = CommunicationUtil.encode(args);
 
-        this.out.write(String.valueOf(encoded.length()));
-        this.out.write(' ');
-        this.out.write(encoded);
-        this.out.write(' ');
-        this.out.flush();
-
-        // destroy process if "normal" exit does not work...
-        IllegalThreadStateException ex = null;
-        int count = 0;
-        do {
-            ex = null;
-            try  {
-                this.process.exitValue();
-            } catch (final IllegalThreadStateException e)  {
-                ex = e;
-                Thread.sleep(1000);
-                count++;
+        try  {
+            if (this.connected)  {
+                this.out.write(String.valueOf(encoded.length()));
+                this.out.write(' ');
+                this.out.write(encoded);
+                this.out.write(' ');
+                this.out.flush();
             }
-        } while ((ex != null) && (count < 10));
-        if (ex != null)  {
-            this.process.destroy();
+        } finally  {
+            // destroy process if "normal" exit does not work...
+            IllegalThreadStateException ex = null;
+            int count = 0;
+            do {
+                ex = null;
+                try  {
+                    this.process.exitValue();
+                } catch (final IllegalThreadStateException e)  {
+                    ex = e;
+                    Thread.sleep(1000);
+                    count++;
+                }
+            } while ((ex != null) && (count < 10));
+            if (ex != null)  {
+                this.process.destroy();
+            }
         }
     }
 
@@ -307,6 +391,174 @@ if (first)  {
             }
         } finally  {
             in.close();
+        }
+    }
+
+    /**
+     * Handler for the error stream. The error stream is read within a separate
+     * thread into {@link #buffer}.
+     */
+    private static final class ErrorStreamHandler
+        extends Thread
+    {
+        /**
+         * Error input stream.
+         */
+        private final InputStream in;
+
+        /**
+         * Used to store already read characters from the
+         * {@link #in error input stream}.
+         */
+        private final StringBuilder buffer = new StringBuilder();
+
+        /**
+         * Initializes the {@in error input stream}.
+         *
+         * @param _in new input stream
+         */
+        public ErrorStreamHandler(final InputStream _in)
+        {
+            this.in = _in;
+        }
+
+        /**
+         * <p>Reads from the {@link #in error input stream} current existing
+         * characters and stores them in {@link #buffer}. If no characters
+         * exists, this thread waits for 1s and starts again.</p>
+         * <p>In the case of an thrown exception (e.g. {@link #in} is closed,
+         * ...), an error will be thrown.</p>
+         */
+        @Override
+        public void run()
+        {
+            try {
+                while (true)  {
+                    synchronized (this.in) {
+                        synchronized(this.buffer)  {
+                            while (this.in.available() > 0)  {
+                                this.buffer.append((char) this.in.read());
+                            }
+                        }
+                    }
+                    Thread.sleep(1000);
+                }
+            } catch (final InterruptedException e) {
+                throw new Error(e);
+            } catch (final IOException e) {
+                throw new Error(e);
+            }
+        }
+
+        /**
+         * Checks if {@link #buffer} is empty.
+         *
+         * @return <i>true</i> if {@link #buffer} contains no character;
+         *          otherwise <i>false</i>
+         *
+         */
+        public boolean isEmpty()
+        {
+            synchronized (this.buffer)  {
+                return this.buffer.length() == 0;
+            }
+        }
+
+        /**
+         * Reads current read buffer. A synchronize on {@link #buffer} is done
+         * so that the method waits until no characters are read anymore by
+         * {@link #run()}.
+         *
+         * @return string
+         * @see #buffer
+         */
+        public String read()
+        {
+            synchronized (this.buffer)  {
+                final String ret = this.buffer.toString();
+                this.buffer.delete(0, this.buffer.length());
+                return ret.toString();
+            }
+        }
+    }
+
+    /**
+     * Handler for the input stream of the process. The input stream is read
+     * within a thread and stored in a buffer.
+     */
+    private static final class InputStreamHandler
+        extends Thread
+    {
+        /**
+         * Input stream of the process.
+         */
+        private final InputStream in;
+
+        /**
+         * Buffer to store read characters.
+         */
+        private final Vector<Integer> buffer = new Vector<Integer>();
+
+        /**
+         * Initializes the input stream handler.
+         *
+         * @param _in       input stream of the process
+         */
+        public InputStreamHandler(final InputStream _in)
+        {
+            this.in = _in;
+        }
+
+        /**
+         * <p>Reads from the {@link #in input stream} current existing
+         * characters and stores them in {@link #buffer}. If no characters
+         * exists, this thread waits for 1s and starts again.</p>
+         * <p>In the case of an thrown exception (e.g. {@link #in} is closed,
+         * ...), an error will be thrown.</p>
+         */
+        @Override()
+        public void run()
+        {
+            try {
+                while (true)  {
+                    while (this.in.available() > 0)  {
+                        this.buffer.add(this.in.read());
+                    }
+                    Thread.sleep(1000);
+                }
+            } catch (final InterruptedException e) {
+                throw new Error(e);
+            } catch (final IOException e) {
+                throw new Error(e);
+            }
+        }
+
+        /**
+         * Checks if a word exists.
+         *
+         * @return <i>true</i> if {@link #buffer} contains a space (and a
+         *         complete word was returned); otherwise <i>false</i>
+         * @see #buffer
+         */
+        public boolean wordExists()
+        {
+            return this.buffer.contains(32);
+        }
+
+        /**
+         * Reads from {@link #buffer} the next word until a space
+         * (character 32) is defined.
+         *
+         * @return read word
+         */
+        public String readWord()
+        {
+            final StringBuilder ret = new StringBuilder();
+            int bck;
+            while ((bck = this.buffer.remove(0)) != 32)  {
+                ret.append((char) bck);
+            }
+            return ret.toString();
         }
     }
 }
--- mxupdate-eclipse/trunk/src/main/java/org/mxupdate/eclipse/mxadapter/connectors/URLConnectorServer.java
+++ mxupdate-eclipse/trunk/src/main/java/org/mxupdate/eclipse/mxadapter/connectors/URLConnectorServer.java
@@ -21,6 +21,7 @@
 package org.mxupdate.eclipse.mxadapter.connectors;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -113,18 +114,20 @@ public class URLConnectorServer
      * Runs the process for the execution of the dispatch processes to the MX
      * database.
      *
-     * @throws IOException              if write into the streams or read from
-     *                                  the streams failed
-     * @throws ClassNotFoundException   if class is not found within encoding /
-     *                                  decoding
-     * @throws InterruptedException     if process is interrupted
-     * @throws MatrixException          is execution failed
+     * @throws Exception {@link IOException} if write into the streams or read
+     *                   from the streams failed,
+     *                   {@link ClassNotFoundException} if class is not found
+     *                   within encoding / decoding,
+     *                   {@link InterruptedException} if process is
+     *                   interrupted,
+     *                   {@link MatrixException} is execution failed
+     *                   and {@link Exception} for all other case
      */
     protected void run()
-        throws IOException, ClassNotFoundException, InterruptedException, MatrixException
+        throws Exception
     {
         while (true)  {
-            final Map<Integer,String> params = CommunicationUtil.<Integer,String>readParams(System.in);
+            final Map<Integer,String> params = this.readParams(System.in);
 
             final String method = params.get(0);
             final String bck;
@@ -132,8 +135,15 @@ public class URLConnectorServer
                 bck = this.executeEncoded(params.get(1), params.get(2), params.get(3));
             } else if ("exit".equals(method))  {
                 break;
-            } else  {
-                bck = null;
+            } else if ("test".equals(method))  {
+                if (!this.connected)  {
+                    this.connect();
+                    bck = "connect";
+                } else  {
+                    bck = "connected";
+                }
+            } else {
+                throw new MatrixException("unknown method '" + method + "'");
             }
 
             final Map<String,String> ret = new HashMap<String,String>();
@@ -204,5 +214,68 @@ public class URLConnectorServer
             throw new MatrixException(mql.getError());
         }
         return mql.getResult();
+    }
+
+    /**
+     * Reads parameters from the <code>_in</code> stream. First the length of
+     * the decoded string is read, then the decoded string itself. This decoded
+     * string is encoded as map and returned.
+     *
+     * @param <KEY>     class for the key
+     * @param <VALUE>   class for the value
+     * @param _in       input stream
+     * @return map with parameters
+     * @throws Exception if parameters could not be read
+     */
+    protected <KEY,VALUE> Map<KEY,VALUE> readParams(final InputStream _in)
+        throws Exception
+    {
+        final int length = Integer.valueOf(this.readOneWord(_in));
+        final String value = this.readOneWord(_in);
+        if (value.length() != length)  {
+            throw new Exception("wrong parameters");
+        }
+        return CommunicationUtil.<Map<KEY,VALUE>>decode(value);
+    }
+
+    /**
+     * Reads one word from the <code>_in</code> stream (means last character is
+     * a space).
+     *
+     * @param _in   input stream
+     * @return read word
+     * @throws IOException              if character could not be read
+     * @throws InterruptedException     if sleep is interrupted while waiting
+     *                                  for new character
+     */
+    protected String readOneWord(final InputStream _in)
+        throws IOException, InterruptedException
+    {
+        final StringBuilder ret = new StringBuilder();
+
+        int ch;
+        while ((ch = (char) this.readChar(_in)) != ' ')  {
+            ret.append((char) ch);
+        }
+        return ret.toString();
+    }
+
+    /**
+     * Reads on character from the <code>_in</code> stream. The method waits
+     * until a character exists.
+     *
+     * @param _in   input stream
+     * @return read character
+     * @throws IOException              if character could not be read
+     * @throws InterruptedException     if sleep is interrupted while waiting
+     *                                  for new character
+     */
+    protected int readChar(final InputStream _in)
+        throws IOException, InterruptedException
+    {
+        while (_in.available() == 0)  {
+            Thread.sleep(1000);
+        }
+        return _in.read();
     }
 }
